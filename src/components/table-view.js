165ddfa7e9fd0bb2f60361089225ff117e4c15ad
Faisal Hakim <faisalhakim47@gmail.com>
2017-01-18 18:35:53 +0700

    table-view enchanment

--- src/components/table-view.js
+++ src/components/table-view.js
@@ -1,4 +1,5 @@
 import { createArrayWithLength } from '../tools/array'
+import { longpress, mergeEvents } from '../tools/events'
 import { isString } from '../tools/typecheck'
 
 export default {
@@ -6,8 +7,12 @@ export default {
   props: {
     items: { type: Array, required: true },
     columnMap: { type: Object, required: true },
+    columnWidth: { type: Array, default: () => [] },
     visibleColumns: { type: Array },
-    indexes: { type: Array }
+    indexes: { type: Array, default: () => [] },
+    limit: { type: Number, default: 10 },
+    pagination: { type: Boolean, default: true },
+    selectable: { type: Boolean, default: false }
   },
   data() {
     const indexes = this.indexes || []
@@ -15,21 +20,29 @@ export default {
       sortBy: null,
       sortDirection: 'asc',
       skip: 0,
-      limit: 10,
       query: '',
-      columnTitles: Object.keys(this.columnMap)
+      columnTitles: Object.keys(this.columnMap),
+      selectedItems: []
     }
   },
+
   computed: {
+    isIndexed() {
+      return !!this.indexes.length
+    },
+
     indexedItems() {
       const indexes = this.indexes || []
-      return this.items.map((item) => {
+      return this.items.map((item, index) => {
         item._fulltext_ = indexes
           .map((key) => item['key'])
           .join(' ')
+        item._key_ = index
+        item._isSelected = false
         return item
       })
     },
+
     sortedItems() {
       if (this.sortBy === null)
         return this.indexedItems
@@ -40,6 +53,7 @@ export default {
         return this.indexedItems
           .sort((a, b) => a[this.sortBy] < a[this.sortBy])
     },
+
     filteredItems() {
       if (!isString(this.query) || this.query === '')
         return this.sortedItems
@@ -48,11 +62,29 @@ export default {
         return filterRex.test(item._fulltext_)
       })
     },
+
     slicedItems() {
+      if (!this.pagination) return this.filteredItems
       return this.filteredItems.slice(this.skip, this.skip + this.limit)
+    },
+
+    isSelecting() {
+      return !!this.selectedItems.length
     }
   },
+
   methods: {
+    toggleSelect(item) {
+      if (!this.selectable) return
+      const index = this.selectedItems.indexOf(item)
+      if (index === -1) {
+        this.selectedItems.push(item)
+        item._isSelected = true
+      } else {
+        this.selectedItems.splice(index, 1)
+        item._isSelected = false
+      }
+    },
     nextPage() {
       if (this.skip + this.limit >= this.filteredItems.length) return
       this.skip += this.limit
@@ -62,27 +94,50 @@ export default {
       this.skip -= this.limit
     }
   },
+
   render(e) {
     console.log('table-view')
     return e('div', { staticClass: 'table-view' }, [
       e('table', { staticClass: 'table' }, [
         e('thead', [
-          e('tr', this.columnTitles.map((columnTitle) => {
-            return e('th', columnTitle)
+          e('tr', this.columnTitles.map((columnTitle, index) => {
+            return e('th', {
+              attrs: { style: this.columnWidth[index] ? `width:${this.columnWidth[index]}px` : null }
+            }, columnTitle)
           }))
         ]),
+
         e('tbody', this.slicedItems.map((item, index) => {
-          return e('tr', this.columnTitles.map((columnTitle) => {
+          return e('tr', {
+            class: { 'is-selected': item._isSelected },
+            key: item._index_,
+            on: mergeEvents([
+              longpress(() => {
+                this.toggleSelect(item)
+              }),
+              {
+                click: (e) => {
+                  if (e.ctrlKey || this.isSelecting) this.toggleSelect(item)
+                }
+              }
+            ])
+          }, this.columnTitles.map((columnTitle) => {
             const column = this.columnMap[columnTitle]
-            if (Array.isArray(column))
-              return e('td', column.map((btn) => {
-                return e('button', { staticClass: 'button' }, [
-                  e('i', {
-                    staticClass: 'icon ' + btn.iconClass,
-                    on: { click: () => btn.callback(item, index) }
-                  }, btn.iconText)
+            if (Array.isArray(column)) {
+              if (this.isSelecting)
+                return e('td', [
+                  e('i', { staticClass: 'material-icons check-icon' }, 'check')
                 ])
-              }))
+              else
+                return e('td', column.map((btn) => {
+                  return e('button', { staticClass: 'button ripple' }, [
+                    e('i', {
+                      staticClass: 'icon ' + btn.iconClass,
+                      on: { click: () => btn.callback(item, index) }
+                    }, btn.iconText)
+                  ])
+                }))
+            }
 
             else if (typeof column === 'function')
               return e('td', column(item, index))
@@ -92,27 +147,30 @@ export default {
           }))
         }))
       ]),
-      e('div', { staticClass: 'table-view-navigate' }, [
-        e('div', { staticClass: 'table-view-navigate-left' }, [
-          e('field', { props: { label: 'Batasi', direction: 'horizontal' } }, [
-            e('input-select', {
-              props: {
-                options: createArrayWithLength(10).map((_, i) => ((i + 1) * 10)),
-                value: this.limit
-              },
-              on: { change: ({ value }) => this.limit = value }
-            }),
-          ])
-        ]),
-        e('div', { staticClass: 'table-view-navigate-right' }, [
-          e('button', { staticClass: 'button', on: { click: this.prevPage } }, [
-            e('i', { staticClass: 'icon material-icons' }, 'navigate_before')
+
+      this.pagination
+        ? e('div', { staticClass: 'table-view-navigate' }, [
+          e('div', { staticClass: 'table-view-navigate-left' }, [
+            e('field', { props: { label: 'Batasi', direction: 'horizontal' } }, [
+              e('input-select', {
+                props: {
+                  options: createArrayWithLength(10).map((_, i) => ((i + 1) * 10)),
+                  value: this.limit
+                },
+                on: { change: ({ value }) => this.limit = value }
+              }),
+            ])
           ]),
-          e('button', { staticClass: 'button', on: { click: this.nextPage } }, [
-            e('i', { staticClass: 'icon material-icons' }, 'navigate_next')
+          e('div', { staticClass: 'table-view-navigate-right' }, [
+            e('button', { staticClass: 'button', on: { click: this.prevPage } }, [
+              e('i', { staticClass: 'icon material-icons' }, 'navigate_before')
+            ]),
+            e('button', { staticClass: 'button', on: { click: this.nextPage } }, [
+              e('i', { staticClass: 'icon material-icons' }, 'navigate_next')
+            ])
           ])
         ])
-      ])
+        : null
     ])
   }
 }
--- src/tools/events.js
+++ src/tools/events.js
@@ -1,15 +1,5 @@
-export function longpress(handler) {
-  let timeout
-  const mousedown = () =>
-    timeout = setTimeout(handler, 500)
-  const mouseup = () =>
-    clearTimeout(timeout)
-  const mousemove = mouseup
-  return { mousedown, mouseup, mousemove }
-}
-
+let currentTarget
 export function doubleclick(handler) {
-  let currentTarget
   let timeout
   const click = (event) => {
     console.log(currentTarget)
@@ -23,6 +13,16 @@ export function doubleclick(handler) {
     }
   }
   return { click }
+}
+
+export function longpress(handler) {
+  let timeout
+  const mousedown = () =>
+    timeout = setTimeout(handler, 500)
+  const mouseup = () =>
+    clearTimeout(timeout)
+  const mousemove = mouseup
+  return { mousedown, mouseup, mousemove }
 }
 
 export function mergeEvents(events) {
