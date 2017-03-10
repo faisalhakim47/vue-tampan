78d17f7aa5ee8fc2b9597cafe4fa902ad23a72a2
Faisal Hakim <faisalhakim47@gmail.com>
2017-03-10 09:38:27 +0700

    update

--- src/tools/data.js
+++ src/tools/data.js
@@ -1,25 +1,44 @@
 import { ensureArrayType } from '../tools/array'
 import { getTampan, whenTampanReady } from '../tampan'
 
+const errorHandlers = []
+
+export function addAsyncRouteDataErrorHandler(errorHandlerFn) {
+  errorHandlers.push(errorHandlerFn)
+}
+
 export function loadAsyncRouteData(dataRequests) {
-  return (destination, origin, next) => {
+  return (destination, origin, next, opt = {}) => {
     const reqs = dataRequests.map((reqObject) => {
       return reqObject.req(destination).catch((error) => {
-        if (typeof reqObject.err === 'function') {
-          reqObject.err(error, destination)
+        const isErrorDefined = typeof reqObject.err === 'function'
+        const errorPromise = isErrorDefined
+          ? reqObject.err(error, destination)
+          : Promise.resolve()
+        const isErrorThenable = !!errorPromise.then
+        const thenableError = (fn) => {
+          isErrorThenable
+            ? errorPromise.then(() => fn(error, destination))
+            : fn(error, destination)
         }
+        errorHandlers.forEach((errorHandlerFn) => thenableError(errorHandlerFn))
       })
     })
     const dataMaps = dataRequests.map((reqObject) => reqObject.map)
     const reqsPromise = Promise.all(reqs)
       .then(datas => {
-        next((vm) => {
+        const callback = (vm) => {
           dataMaps.forEach((asyncDataFactory, i) => {
             const data = datas[i]
             if (typeof data !== 'object') return
-            asyncDataFactory(vm, datas[i])
+            asyncDataFactory(vm, data)
           })
-        })
+        }
+        next(
+          opt.vm
+            ? callback(vm)
+            : callback
+        )
       })
     whenTampanReady()
       .then(({ tampan }) => {
