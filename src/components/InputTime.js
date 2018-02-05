8c6d4904e6d5c4661c11d22a405d9056c5f3ed36
Faisal Hakim <faisalhakim47@gmail.com>
2018-02-05 15:38:15 +0700

    add disabled prop in input-time

--- src/components/InputTime.js
+++ src/components/InputTime.js
@@ -6,7 +6,8 @@ import Column from './Column.js'
 
 export default {
   props: {
-    value: { type: Date, default: () => new Date() }
+    value: { type: Date, default: () => new Date() },
+    disabled: { type: Boolean, default: false },
   },
 
   components: {
@@ -47,6 +48,7 @@ export default {
     value(newValue) {
       this.ISO8601Time = toISO8601(new Date(newValue), { includeDate: false, includeTime: true })
       this.date = dateToObject(new Date(newValue))
+      console.log(this.date.hour)
     },
   },
 
@@ -56,12 +58,12 @@ export default {
       <Row>
         <Column :width="{ sm: 1/2 }">
           <Field>
-            <input type="number" min="0" max="23" v-model.number="date.hour" @input="input">
+            <input type="number" min="0" max="23" v-model.number="date.hour" @input="input" :disabled="disabled">
           </Field>
         </Column>
         <Column :width="{ sm: 1/2 }">
           <Field>
-            <input type="number" min="0" max="59" v-model.number="date.minute" @input="input">
+            <input type="number" min="0" max="59" v-model.number="date.minute" @input="input" :disabled="disabled">
           </Field>
         </Column>
       </Row>
